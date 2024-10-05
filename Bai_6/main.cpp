778a96264aad3851bc8b335aebfdfeae8bb9b4b2
Glasspham <hathimongthu1980@gmail.com>
2024-10-05 16:54:42 +0700

    Fix code representation

--- Bai_6/main.cpp
+++ Bai_6/main.cpp
@@ -1,55 +1,65 @@
 /*
-a. Hiện thực Lớp Shape, Rectangle, và Triangle như sau, chạy thử và giải thích kết quả xuất ra màn hình.
+! Đề bài
+a. Hiện thực Lớp Shape, Rectangle và Triangle như sau, chạy thử và giải thích kết quả xuất ra màn hình.
 b. Chỉnh sửa để kết quả xuất ra là diện tích của các hình thuộc lớp con tương ứng.
 */
-#include<iostream>
+
+#include <iostream>
 using namespace std;
 
-class Shape{
+class Shape
+{
 protected:
     int width, height;
+
 public:
-    Shape(int a = 0, int b = 0){
+    Shape(int a = 0, int b = 0)
+    {
         width = a;
         height = b;
     }
 
-    int area(){ 
+    int area()
+    {
         cout << "Parent class area :" << endl;
         return 0;
     }
 };
 
-class Rectangle : public Shape{
+class Rectangle : public Shape
+{
 public:
-    Rectangle(int a = 0,int b = 0):Shape(a, b){}
+    Rectangle(int a = 0, int b = 0) : Shape(a, b) {}
 
-    int area (){
-        cout <<"Rectangle class area :"<<endl;
-        return(width * height);
+    int area()
+    {
+        cout << "Rectangle class area :" << endl;
+        return (width * height);
     }
 };
 
-class Triangle : public Shape{
+class Triangle : public Shape
+{
 public:
-Triangle(int a = 0,int b = 0):Shape(a, b){}
-    int area (){
-        cout <<"Triangle class area :"<<endl;
-        return(width * height / 2);
+    Triangle(int a = 0, int b = 0) : Shape(a, b) {}
+    int area()
+    {
+        cout << "Triangle class area :" << endl;
+        return (width * height / 2);
     }
 };
 
-// Main function for the program
-int main(){
-    Shape*shape;
-    Rectangle rec(10,7);
-    Triangle  tri(10,5);
+int main()
+{
+    Shape *shape;
+    Rectangle rec(10, 7);
+    Triangle tri(10, 5);
     // store the address of Rectangle
-    shape =&rec;
+    shape = &rec;
     // call rectangle area.
     shape->area();
     // store the address of Triangle
-    shape =&tri;
+    shape = &tri;
     // call triangle area.
     shape->area();
     return 0;
@@ -64,54 +74,61 @@ Khi gọi shape->area(), vì area() trong lớp Shape không phải là hàm ả
 Mặc dù shape đang trỏ tới đối tượng của Rectangle và Triangle, nhưng do cơ chế liên kết tĩnh (static binding), hàm area() của lớp cơ sở Shape được gọi.
 */
 
-
 /*
 Phần b: Chỉnh sửa để Kết quả Xuất ra là Diện Tích của Các Hình Thuộc Lớp Con Tương Ứng
 Để kết quả xuất ra là diện tích của các hình thuộc lớp con, ta cần sử dụng hàm ảo (virtual function) trong lớp cơ sở Shape.
 Chỉnh sửa mã nguồn:
 */
 
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-class Shape{
+class Shape
+{
 protected:
     int width, height;
+
 public:
-    Shape(int a = 0, int b = 0){
+    Shape(int a = 0, int b = 0)
+    {
         width = a;
         height = b;
     }
 
-    virtual int area(){  // Sử dụng từ khóa virtual
+    virtual int area()
+    {
         cout << "Parent class area :" << endl;
         return 0;
     }
 };
 
-class Rectangle : public Shape{
+class Rectangle : public Shape
+{
 public:
-    Rectangle(int a = 0, int b = 0): Shape(a, b){}
+    Rectangle(int a = 0, int b = 0) : Shape(a, b) {}
 
-    int area (){
+    int area()
+    {
         cout << "Rectangle class area :" << endl;
-        return(width * height);
+        return (width * height);
     }
 };
 
-class Triangle : public Shape{
+class Triangle : public Shape
+{
 public:
-    Triangle(int a = 0, int b = 0): Shape(a, b){}
+    Triangle(int a = 0, int b = 0) : Shape(a, b) {}
 
-    int area (){
+    int area()
+    {
         cout << "Triangle class area :" << endl;
-        return(width * height / 2);
+        return (width * height / 2);
     }
 };
 
-// Main function for the program
-int main(){
-    Shape* shape;
+int main()
+{
+    Shape *shape;
     Rectangle rec(10, 7);
     Triangle tri(10, 5);
     // store the address of Rectangle
