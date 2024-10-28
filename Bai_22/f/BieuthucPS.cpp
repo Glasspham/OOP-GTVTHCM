202ebe1ca30ab90849badb57100dd7bfcc4552f2
Glasspham <hathimongthu1980@gmail.com>
2024-10-28 16:54:59 +0700

    Fix

--- Bai_22/f/BieuthucPS.cpp
+++ Bai_22/f/BieuthucPS.cpp
@@ -1,17 +1,28 @@
 #include "BieuthucPS.h"
 
 BieuthucPS::BieuthucPS(int level) : Bieuthuc(level) {
-    p1 = Fraction(Bieuthuc(level).giatri(), Bieuthuc(level).giatri());
-    p2 = Fraction(Bieuthuc(level).giatri(), Bieuthuc(level).giatri());
-    if(level == 1) pheptoan = '+';
-    else if(level == 2) pheptoan = (rand() % 2 == 0) ? '+' : '-';
-    else if(level == 3) {
-        int random =  rand() % 3;
-        pheptoan = (random == 0) ? '+' : (random == 1) ? '-' : '*';
-    } else if(level == 4) pheptoan = (rand() % 2 == 0) ? '+' : '-';
-    else {
-        int random =  rand() % 3;
-        pheptoan = (random == 0) ? '+' : (random == 1) ? '-' : '*';
+    if(level == 1) {
+        p1 = Fraction(rand() % 11, rand() % 11);
+        p2 = Fraction(rand() % 11, rand() % 11);
+        pheptoan = '+';
+    } else if(level == 2) {
+        p1 = Fraction(rand() % 11, rand() % 11);
+        p2 = Fraction(rand() % 11, rand() % 11);
+        pheptoan = (rand() % 2 + 1 == 1) ? '+' : '-';
+    } else if(level == 3) {
+        p1 = Fraction(rand() % 11, rand() % 11);
+        p2 = Fraction(rand() % 11, rand() % 11);
+        int random =  rand() % 3 + 1;
+        pheptoan = (random == 1) ? '+' : (random == 2) ? '-' : '*';
+    } else if(level == 4) {
+        p1 = Fraction(rand() % 101, rand() % 101);
+        p2 = Fraction(rand() % 101, rand() % 101);
+        pheptoan = (rand() % 2 + 1 == 1) ? '+' : '-';
+    } else {
+        p1 = Fraction(rand() % 101, rand() % 101);
+        p2 = Fraction(rand() % 101, rand() % 101);
+        int random =  rand() % 3 + 1;
+        pheptoan = (random == 1) ? '+' : (random == 2) ? '-' : '*';
     }
 }
 
